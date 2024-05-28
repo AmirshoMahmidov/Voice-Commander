f10e6ffcb3146ebf8b260ca30fd2e886c726c3a7
arne_ <darsamirmahmudov@gmail.com>
2024-05-29 04:58:55 +0500

    Fnital Commit

--- app/src/main/java/com/example/voicecommander_1/MainActivity.kt
+++ app/src/main/java/com/example/voicecommander_1/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieDrawable
 import com.example.voicecommander_1.databinding.ActivityMainBinding
 import java.util.Locale
 
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private var isFlashOn = false // Переменная для отслеживания состояния фонарика
     private lateinit var cameraManager: CameraManager // Переменная для управления камерой
     private var cameraId: String? = null // Идентификатор камеры
-
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // Инициализация View Binding
@@ -35,18 +37,37 @@ class MainActivity : AppCompatActivity() {
         binding.btnMic.setOnClickListener {
             onClickMic() // Вызов функции для обработки нажатия на кнопку
         }
+
+        // Запуск анимации при создании активности с повторением
+        binding.lottieAnimationView.setAnimation("animation.json")
+        binding.lottieAnimationView.repeatCount = LottieDrawable.INFINITE // Повторение анимации бесконечно
+        binding.lottieAnimationView.playAnimation()
+
+        // Настройка фоновой анимации
+        binding.lottieBackgroundAnimationView.setAnimation("background_animation.json")
+        binding.lottieBackgroundAnimationView.repeatCount = LottieDrawable.INFINITE
+        binding.lottieBackgroundAnimationView.playAnimation()
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bam)
     }
 
     // Функция для обработки нажатия на кнопку микрофона
     private fun onClickMic() {
+        // Уменьшение громкости музыки до 30% от максимальной
+        mediaPlayer.setVolume(0.0f, 0.0f)
+
         // Создание интента для распознавания речи
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(
             RecognizerIntent.EXTRA_LANGUAGE_MODEL,
             RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+
         ) // Установка параметров распознавания речи, модель языка свободной формы
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault()) // Установка языка по умолчанию
         startActivityForResult(intent, 10) // Запуск активности распознавания речи с ожиданием результата
+
+
     }
 
     // Обработка результата распознавания речи
@@ -65,12 +86,10 @@ class MainActivity : AppCompatActivity() {
                         text[0].contains("яблоко", ignoreCase = true) -> {
                             binding.imageView.setImageResource(R.drawable.yab) // Установка изображения яблока
                             binding.imageView.visibility = View.VISIBLE // Отображение ImageView
-
                         }
                         text[0].contains("абу", ignoreCase = true) -> {
                             binding.imageView.setImageResource(R.drawable.abu) // Установка изображения груши
                             binding.imageView.visibility = View.VISIBLE // Отображение ImageView
-
                         }
                         text[0].contains("арбуз", ignoreCase = true) -> {
                             binding.imageView.setImageResource(R.drawable.arb) // Установка изображения груши
@@ -86,10 +105,33 @@ class MainActivity : AppCompatActivity() {
                         text[0].contains("куш", ignoreCase = true) -> {
                             toggleFlashlight(false) // Выключение фонарика
                         }
+                        text[0].contains("музыка", ignoreCase = true) -> {
+                            mediaPlayer.start() // Воспроизведение музыки
+                            Toast.makeText(this, "Музыка играет", Toast.LENGTH_SHORT).show()
+                        }
+                        text[0].contains("стоп", ignoreCase = true) -> {
+                            mediaPlayer.pause() // Пауза воспроизведения музыки
+                            mediaPlayer.seekTo(0) // Возврат на начало трека
+                            Toast.makeText(this, "Музыка остановлена", Toast.LENGTH_SHORT).show()
+                        }
+                        text[0].contains("пауза", ignoreCase = true) -> {
+                            mediaPlayer.pause() // Пауза воспроизведения музыки
+                            // Возврат на начало трека
+                            Toast.makeText(this, "Музыка остановлена", Toast.LENGTH_SHORT).show()
+                        }
+                        text[0].contains("выход", ignoreCase = true) -> {
+                            finish()
+
+                        }
+
                         else -> {
-                            binding.imageView.visibility = View.GONE // Скрытие ImageView, если текст не соответствует ни одному условию
+                            binding.imageView.visibility = View.INVISIBLE // Скрытие ImageView, если текст не соответствует ни одному условию
+
                         }
                     }
+
+                    // Восстановление громкости музыки до максимальной после обработки команды
+                    mediaPlayer.setVolume(1f, 1f)
                 }
             }
         }
